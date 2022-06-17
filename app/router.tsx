bce3989f7677b231e8091d5ef52f75ec6e747544
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-06-18 04:50:10 +0700

    Setup bottomsheet

--- app/router.tsx
+++ app/router.tsx
@@ -3,10 +3,12 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import TaskContext from './models';
 import {HomeScreen, PartScreen, ReportScreen, ServiceScreen} from './screens';
 const {RealmProvider} = TaskContext;
@@ -21,20 +23,30 @@ export type MainStackParamList = {
 const MainStack = createNativeStackNavigator<MainStackParamList>();
 export default () => {
   return (
-    <RealmProvider>
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer theme={DefaultTheme}>
-          <MainStack.Navigator
-            initialRouteName="HomeScreen"
-            screenOptions={{headerShown: false}}>
-            <MainStack.Screen name="HomeScreen" component={HomeScreen} />
-            <MainStack.Screen name="PartScreen" component={PartScreen} />
-            <MainStack.Screen name="ReportScreen" component={ReportScreen} />
-            <MainStack.Screen name="ServiceScreen" component={ServiceScreen} />
-          </MainStack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
-    </RealmProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <BottomSheetModalProvider>
+        <RealmProvider>
+          <SafeAreaView style={styles.container}>
+            <NavigationContainer theme={DefaultTheme}>
+              <MainStack.Navigator
+                initialRouteName="HomeScreen"
+                screenOptions={{headerShown: false}}>
+                <MainStack.Screen name="HomeScreen" component={HomeScreen} />
+                <MainStack.Screen name="PartScreen" component={PartScreen} />
+                <MainStack.Screen
+                  name="ReportScreen"
+                  component={ReportScreen}
+                />
+                <MainStack.Screen
+                  name="ServiceScreen"
+                  component={ServiceScreen}
+                />
+              </MainStack.Navigator>
+            </NavigationContainer>
+          </SafeAreaView>
+        </RealmProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
 
