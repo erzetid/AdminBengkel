d47a6848cda4f9f01ec6573ccba39f8c23628541
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-06-18 04:53:25 +0700

    Add interface for part screen

--- app/screens/interface.ts
+++ app/screens/interface.ts
@@ -13,3 +13,42 @@ export interface HomeScreenProps extends ScreenProps {}
 export interface PartScreenProps extends ScreenProps {}
 export interface ReportScreenProps extends ScreenProps {}
 export interface ServiceScreenProps extends ScreenProps {}
+export interface PartDetail {
+  id: string;
+  name: string;
+  code: string;
+  category: string;
+  description: string;
+  quantity: number;
+  price: number;
+  image: any;
+  location: string;
+}
+
+export interface BottomSheetDetailProps extends ScreenProps {
+  detail: PartDetail | undefined;
+}
+
+export interface SecondHeaderProps extends ScreenProps {
+  title: string;
+}
+
+export interface SearchBarProps {
+  title: string;
+  onPress: () => void;
+  onChangeText?: (text: string) => void;
+}
+
+export interface BottomSheetStockProps {
+  item: PartDetail | undefined;
+  stock: number;
+  setStock: (stock: number) => void;
+  onSave: (id: string) => void;
+  onChangeText: (text: string) => void;
+}
+
+export interface PartListProps {
+  handleItemPress: (value: PartDetail) => void;
+  handleStockModalPress: (value: PartDetail) => void;
+  data: PartDetail[];
+}
