4f53213f6c3c9a9ccc0210f7cd663475dfb8a341
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-06-18 04:55:04 +0700

    Add yellow color

--- app/constant/theme.ts
+++ app/constant/theme.ts
@@ -8,8 +8,12 @@ export const color = {
   purple: '#32089b',
   lightPurple: '#5267BE',
   gray: '#B5B5B5',
+  darkGray: '#686666',
   white: '#FFFFFF',
   black: '#000000',
   primary: '#FFFFFF',
   secondary: '#000000',
+  lightBlue: '#56CCF2',
+  yellow: '#FFBC47',
+  lightGray: '#F3F3F1',
 };
