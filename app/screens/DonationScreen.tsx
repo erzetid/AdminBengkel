d1f4bef0f69e63363ba5400e37072811cdd4acd3
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-06-28 00:24:29 +0700

    finish

--- app/screens/DonationScreen.tsx
+++ app/screens/DonationScreen.tsx
@@ -3,20 +3,118 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import React, {FC} from 'react';
-import {StyleSheet} from 'react-native';
+import React, {FC, useCallback, useEffect, useState} from 'react';
+import {Linking, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  AdEventType,
+  RewardedAd,
+  RewardedAdEventType,
+  TestIds,
+} from 'react-native-google-mobile-ads';
 import SecondBackground from '../components/SecondBackground';
 import SecondHeader from '../components/SecondHeader';
+import {color} from '../constant/theme';
 import {DonationScreenProps} from './interface';
 
+const adUnitId = __DEV__
+  ? TestIds.REWARDED
+  : 'ca-app-pub-5427566701323504/5692918375';
+let rewarded: RewardedAd;
 const DonationScreen: FC<DonationScreenProps> = ({navigation}) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    console.log('init');
+    rewarded = RewardedAd.createForAdRequest(adUnitId, {
+      requestNonPersonalizedAdsOnly: true,
+    });
+
+    const unsubscribeLoaded = rewarded.addAdEventListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        setLoaded(true);
+      },
+    );
+    const unsubscribeEarned = rewarded.addAdEventListener(
+      AdEventType.CLOSED,
+      () => {
+        navigation?.navigate('HomeScreen');
+      },
+    );
+    rewarded.addAdEventListener(AdEventType.ERROR, () => {
+      navigation?.navigate('HomeScreen');
+    });
+
+    // Start loading the rewarded ad straight away
+    rewarded.load();
+    return () => {
+      unsubscribeLoaded();
+      unsubscribeEarned();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleOnClickAds = useCallback(() => {
+    rewarded.show();
+  }, []);
+
   return (
     <SecondBackground>
       <SecondHeader title="Donasi" navigation={navigation} />
+      <View style={styles.content}>
+        <TouchableOpacity
+          style={loaded ? styles.btnActive : styles.btnNotActive}
+          onPress={handleOnClickAds}
+          disabled={!loaded}>
+          <Text style={styles.text}>TONTON IKLAN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnActive}
+          onPress={() => {
+            const url = 'https://saweria.co/andrifahrizal';
+            Linking.canOpenURL(url).then(supported => {
+              if (supported) {
+                Linking.openURL(url);
+              } else {
+                console.log('Donasi gagal');
+              }
+            });
+          }}>
+          <Text style={styles.text}>SAWERIA</Text>
+        </TouchableOpacity>
+      </View>
     </SecondBackground>
   );
 };
 
 export default DonationScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: color.white,
+    borderRadius: 5,
+    justifyContent: 'center',
+  },
+  btnActive: {
+    backgroundColor: color.lightBlue,
+    padding: 15,
+    borderRadius: 5,
+    elevation: 1,
+    marginVertical: 5,
+  },
+  btnNotActive: {
+    backgroundColor: color.darkGray,
+    padding: 15,
+    borderRadius: 5,
+    elevation: 1,
+    marginVertical: 5,
+  },
+  text: {
+    color: color.white,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
