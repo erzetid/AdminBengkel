3caf897dec851ded57ec4e78fd6f876b2b7c278d
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-06-28 09:46:38 +0700

    centralize

--- app/database/Part.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 fahrizalm14
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-import {PartDetail} from '../screens/interface';
-import Database from './DBHelper';
-
-export const parts = new Database<PartDetail>('parts');
--- app/database/Vehicle.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 fahrizalm14
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-import {Vehicle} from '../model/Vehicle';
-import Database from './DBHelper';
-
-export const vehicles = new Database<Vehicle>('vehicle');
