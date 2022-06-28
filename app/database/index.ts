a84d875a8a7a3d28a5caa12243f273d423c0a4b0
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-06-28 09:48:47 +0700

    update database

--- app/database/index.ts
+++ app/database/index.ts
@@ -3,11 +3,21 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import {parts} from './Part';
-import {vehicles} from './Vehicle';
+import {ICustomer} from '../model/Customer';
+import {INote} from '../model/Note';
+import {Vehicle} from '../model/Vehicle';
+import {PartDetail} from '../screens/interface';
+import Database from './DBHelper';
+
+const parts = new Database<PartDetail>('parts');
+const vehicles = new Database<Vehicle>('vehicle');
+const customers = new Database<ICustomer>('customer');
+const notes = new Database<INote>('note');
 
 const LocalDB = {
   parts,
   vehicles,
+  customers,
+  notes,
 };
 export default LocalDB;
---
+++ app/model/Customer.ts
@@ -0,0 +1,43 @@
+// Copyright (c) 2022 fahrizalm14
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export interface ICustomer {
+  name: string;
+  phone: string;
+  address: string;
+}
+
+export class Customer {
+  private name: string;
+  private phone = '';
+  private address = '';
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  setPhone(phone: string): Customer {
+    this.phone = phone;
+    return this;
+  }
+
+  setAddress(address: string): Customer {
+    this.address = address;
+    return this;
+  }
+  getName(): string {
+    return this.name;
+  }
+  getPhone(): string {
+    return this.phone;
+  }
+  getAddress(): string {
+    return this.address;
+  }
+
+  toJSON(): ICustomer {
+    return {name: this.name, phone: this.phone, address: this.address};
+  }
+}
